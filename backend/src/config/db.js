87fc3134e7eb5a3072687d7323d635bed8886c5b
NDHari36 <nguyenduchai362005@gmail.com>
2026-05-13 22:54:49 +0700

    fix routing

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,14 @@
-require('dotenv').config(); 
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'TechZone',
-    waitForConnections: true,
-    connectionLimit: 10, // Tối đa 10 kết nối cùng lúc
-    queueLimit: 0
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "",
+  database: process.env.DB_NAME || "TechZone",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool.promise();
