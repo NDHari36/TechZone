6cfdfac6878f724e33607af1abc8d457e0cc2db9
NDHari36 <nguyenduchai362005@gmail.com>
2026-05-15 19:47:06 +0700

    commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -14,6 +15,8 @@ const pool = mysql.createPool({
   ssl: {
     rejectUnauthorized: false,
   },
+
+  connectTimeout: 10000,
 });
 
 module.exports = pool.promise();
