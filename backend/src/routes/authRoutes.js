d57a41990dae3448f1b82ba6be7dc843f247a558
NDHari36 <nguyenduchai362005@gmail.com>
2026-06-08 16:48:29 +0700

    fix routes auth

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const authController = require("../controllers/authController");
 
 router.post("/signup", authController.register);
 router.post("/signin", authController.login);
+router.post("/refresh-token", authController.refreshToken);
 router.post("/logout", authController.logout);
 router.put("/reset-password-default/:id", authController.resetPasswordDefault);
 
